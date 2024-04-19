d6a151609b5b570f27875ae3fd145527a79b1b5f
Spandcynd <spandcynd@ukr.net>
2024-04-19 13:47:20 +0300

    adaptive done

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-const dropdown = document.querySelector('.form-dropdown-item');
-const dropdownBody = document.querySelector('.form-dropdown-item__body');
-const dropdownHeader = document.querySelector('.form-dropdown-item__header');
+// dropdown in contact-me section form
+(function () {
+  const dropdown = document.querySelector('.form-dropdown-item');
+  const dropdownBody = document.querySelector('.form-dropdown-item__body');
+  const dropdownHeader = document.querySelector('.form-dropdown-item__header');
 
-dropdown.addEventListener('click', (e) => {
-  if (!e.target.closest('.form-dropdown-item__body')) e.currentTarget.classList.toggle('expanded');
-});
+  dropdown.addEventListener('click', (e) => {
+    if (!e.target.closest('.form-dropdown-item__body'))
+      e.currentTarget.classList.toggle('expanded');
+  });
 
-dropdownBody.addEventListener('click', (e) => {
-  const inputs = dropdownBody.querySelectorAll('input');
-  const input = Array.from(inputs).find((input) => input === e.target);
-  if (input) {
-    dropdownHeader.textContent = input.previousElementSibling.textContent;
-    dropdown.classList.remove('expanded');
-  }
-});
+  dropdownBody.addEventListener('click', (e) => {
+    const inputs = dropdownBody.querySelectorAll('input');
+    const input = Array.from(inputs).find((input) => input === e.target);
+    if (input) {
+      dropdownHeader.textContent = input.previousElementSibling.textContent;
+      dropdown.classList.remove('expanded');
+    }
+  });
+})();
+
+// main navigation burger handling
+(function () {
+  const burgerController = document.querySelector('.main-nav-burger__controller');
+  const mainNav = document.querySelector('.main-nav-burger__main-nav');
+
+  burgerController.addEventListener('click', (e) => {
+    mainNav.classList.toggle('active');
+  });
+})();
